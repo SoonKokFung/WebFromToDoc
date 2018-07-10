363780f71e97c1efb1d6daa5910b7f772ec1a42d
KokFung <KokFung@KOKFUNG-PC>
2018-07-10 15:14:05 +0800

    Add project files.

---
+++ WebFromToDoc/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebFromToDoc.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Name
+            <asp:TextBox runat="server" ID="txtName" required />
+            <br />
+            Title
+            <asp:TextBox runat="server" ID="txtTitle" required />
+            <br />
+            <asp:Button runat="server" Text="Ok" OnClick="Unnamed_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebFromToDoc/WebForm1.aspx.cs
@@ -0,0 +1,53 @@
+﻿
+using Spire.Doc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFromToDoc
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed_Click(object sender, EventArgs e)
+        {
+            createDoc(Server.MapPath("Template/Example.docx"));
+        }
+        private void createDoc(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                Document document = new Document();
+                document.LoadFromFile(filePath);
+
+                document.Replace("<Title>", txtTitle.Text, true, true);
+                document.Replace("<name>", txtName.Text, true, true);
+
+                document.Replace("<CompanyName>", "冰冰无限公司", true, true);
+                document.Replace("<Date>", DateTime.Now.ToString("dd-MMM-yyyy HH:mm:ss").ToString(), true, true);
+
+                string tempPath = Server.MapPath("Temporarily/" + DateTime.Now.ToString("ddmmyyyyhhmmssffffff") + ".docx");
+                document.SaveToFile(tempPath, FileFormat.Docx);
+
+                //let the user download the doc
+                Response.ContentType = "Application/msword";
+                Response.AddHeader("Content-Disposition", "attachment;filename=" + tempPath);
+                Response.TransmitFile(Path.Combine(tempPath.ToString()));
+                Response.Flush();
+                File.Delete(tempPath);
+                Response.End();
+            }
+
+        }
+    }
+}
