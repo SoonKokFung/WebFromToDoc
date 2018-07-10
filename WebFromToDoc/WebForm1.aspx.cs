dd9d608a75026a85affec3a028f72d54c41cdcce
KokFung <KokFung@KOKFUNG-PC>
2018-07-10 15:16:52 +0800

    comment

--- WebFromToDoc/WebForm1.aspx.cs
+++ WebFromToDoc/WebForm1.aspx.cs
@@ -21,25 +21,30 @@ namespace WebFromToDoc
 
         protected void Unnamed_Click(object sender, EventArgs e)
         {
+            //load the template
             createDoc(Server.MapPath("Template/Example.docx"));
         }
         private void createDoc(string filePath)
-        {
+        {   //check the template is available
             if (File.Exists(filePath))
             {
+                //load the doc
                 Document document = new Document();
                 document.LoadFromFile(filePath);
 
+                //find and replace the content
                 document.Replace("<Title>", txtTitle.Text, true, true);
                 document.Replace("<name>", txtName.Text, true, true);
 
+                //find and replace the header & footer
                 document.Replace("<CompanyName>", "冰冰无限公司", true, true);
                 document.Replace("<Date>", DateTime.Now.ToString("dd-MMM-yyyy HH:mm:ss").ToString(), true, true);
 
+                //save the doc in temporarily folder
                 string tempPath = Server.MapPath("Temporarily/" + DateTime.Now.ToString("ddmmyyyyhhmmssffffff") + ".docx");
                 document.SaveToFile(tempPath, FileFormat.Docx);
 
-                //let the user download the doc
+                //let the user download and delete it the doc
                 Response.ContentType = "Application/msword";
                 Response.AddHeader("Content-Disposition", "attachment;filename=" + tempPath);
                 Response.TransmitFile(Path.Combine(tempPath.ToString()));
